d8b1d8be9daa5847a34f2625dd6762ed89eb2fcd
Abdelaziz Bahoumi <abdobm2811@gmail.com>
2025-12-08 18:34:42 +0100

    fixed case [echo -n sa | ./checker 2 1] should print Error

--- bonus/srcs/process_operations.c
+++ bonus/srcs/process_operations.c
@@ -6,35 +6,35 @@
 /*   By: abahoumi <abahoumi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/03 16:35:08 by abahoumi          #+#    #+#             */
-/*   Updated: 2025/12/08 17:39:08 by abahoumi         ###   ########.fr       */
+/*   Updated: 2025/12/08 18:31:26 by abahoumi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "checker_bonus.h"
 
-static int	is_correct_format(char *operation, size_t operation_len)
+static int	is_correct_format(char *operation)
 {
-	if (!ft_strncmp(operation, "sa\n", operation_len))
+	if (!ft_strncmp(operation, "sa\n", 3))
 		return (1);
-	else if (!ft_strncmp(operation, "sb\n", operation_len))
+	else if (!ft_strncmp(operation, "sb\n", 3))
 		return (1);
-	else if (!ft_strncmp(operation, "ss\n", operation_len))
+	else if (!ft_strncmp(operation, "ss\n", 3))
 		return (1);
-	else if (!ft_strncmp(operation, "pa\n", operation_len))
+	else if (!ft_strncmp(operation, "pa\n", 3))
 		return (1);
-	else if (!ft_strncmp(operation, "pb\n", operation_len))
+	else if (!ft_strncmp(operation, "pb\n", 3))
 		return (1);
-	else if (!ft_strncmp(operation, "ra\n", operation_len))
+	else if (!ft_strncmp(operation, "ra\n", 3))
 		return (1);
-	else if (!ft_strncmp(operation, "rb\n", operation_len))
+	else if (!ft_strncmp(operation, "rb\n", 3))
 		return (1);
-	else if (!ft_strncmp(operation, "rr\n", operation_len))
+	else if (!ft_strncmp(operation, "rr\n", 3))
 		return (1);
-	else if (!ft_strncmp(operation, "rra\n", operation_len))
+	else if (!ft_strncmp(operation, "rra\n", 4))
 		return (1);
-	else if (!ft_strncmp(operation, "rrb\n", operation_len))
+	else if (!ft_strncmp(operation, "rrb\n", 4))
 		return (1);
-	else if (!ft_strncmp(operation, "rrr\n", operation_len))
+	else if (!ft_strncmp(operation, "rrr\n", 4))
 		return (1);
 	else
 		return (0);
@@ -63,7 +63,7 @@ t_list	*process_operations(int *is_error)
 	line = get_next_line(0);
 	while (line)
 	{
-		if (!is_correct_format(line, ft_strlen(line)))
+		if (!is_correct_format(line))
 			return (*is_error = 1, free(line), free_list(operations), NULL);
 		ft_lstadd_back(&operations, ft_lstnew((void *)ft_strdup(line)));
 		free(line);
