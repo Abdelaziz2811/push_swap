afc09e19d422be4db8f08e3d2b7dfa053d275753
Abdelaziz Bahoumi <abdobm2811@gmail.com>
2025-12-08 18:22:21 +0100

    fix stack b not empty issue

--- bonus/srcs/checker_bonus.c
+++ bonus/srcs/checker_bonus.c
@@ -6,7 +6,7 @@
 /*   By: abahoumi <abahoumi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/03 14:08:41 by abahoumi          #+#    #+#             */
-/*   Updated: 2025/12/08 17:44:02 by abahoumi         ###   ########.fr       */
+/*   Updated: 2025/12/08 18:18:59 by abahoumi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,20 +20,19 @@ int	main(int argc, char **argv)
 	t_list	*operations;
 	int		is_error;
 
+	a = NULL;
+	b = NULL;
 	if (argc == 1)
 		return (0);
 	args = get_args(argv);
 	if (!valid_format(args) || !*argv[1] || is_empty_or_whitespace(argv))
 		return (free_args(args), ft_putendl_fd("Error", 2), 0);
 	init_stack(&a, args);
-	if (!stack_sorted(a))
-	{
-		operations = process_operations(&is_error);
-		if (!operations && is_error)
-			return (free_memory(args, &a, &b), ft_putendl_fd("Error", 2), 0);
-		execute_operations(operations, &a, &b);
-	}
-	if (stack_sorted(a))
+	operations = process_operations(&is_error);
+	if (!operations && is_error)
+		return (free_memory(args, &a, &b), ft_putendl_fd("Error", 2), 0);
+	execute_operations(operations, &a, &b);
+	if (stack_sorted(a) && !stack_size(b))
 		ft_printf("OK\n");
 	else
 		ft_printf("KO\n");
